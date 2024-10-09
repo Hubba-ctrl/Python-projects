15c0ffd1be8d578f535826372e486b294edc708c
hubba-ctrl <alemobaraki@gmail.com>
2024-10-09 19:18:33 +0200

    added some changes

--- keep_playing_flask.py
+++ keep_playing_flask.py
@@ -1,8 +1,17 @@
-from markupsafe import escape
+# from markupsafe import escape
 
-from flask import Flask, url_for
+# from flask import Flask, url_for, request
 
-app = Flask(__name__)
+
+
+# app = Flask(__name__)
+
+""" Flask quickstart variable rules"""
+# from markupsafe import escape
+
+# from flask import Flask, url_for, request
+
+
 
 # @app.route('/user/<username>')
 # def show_user_profile(username):
@@ -15,6 +24,12 @@ app = Flask(__name__)
 # @app.route("/path/<path:subpath>")
 # def show_subpath(subpath):
 #     return f"Subpath {escape(subpath)}"
+""" Routing text quickstart flask  """
+# from markupsafe import escape
+
+# from flask import Flask, url_for, request
+
+
 
 # @app.route("/projects/")
 # def projects():
@@ -24,10 +39,16 @@ app = Flask(__name__)
 # def status():
 #     return "The status page"
 
-@app.route('/')
-def sandra():
-    return "Sandra Mobaraki är cool. hon är min syster och bror, hon kan ha mördat folk i sin dröm, men det var bara en dröm"
+""" Quickstart flask test ur building """
+# from markupsafe import escape
 
+# from flask import Flask, url_for, request
+
+
+
+# @app.route('/')
+# def index():
+#     return "index"
 # @app.route('/login')
 # def login():
 #     return "login"
@@ -35,15 +56,39 @@ def sandra():
 # def profile(username):
 #     return f"{username}\'s profile" 
 
+# with app.test_request_context():
+#     print (url_for('index'))
+#     print (url_for('login'))
+#     print (url_for('login',next='/'))
+#     print (url_for('profile' ,username='John Doe'))
+
+""" quickstart http methods """
+# from markupsafe import escape
+
+# from flask import Flask, url_for, request
 
 
-if __name__ == "__main__":
-    # with app.test_request_context():
-    #     print (url_for('index'))
-    #     print (url_for('login'))
-    #     print (url_for('login',next='/'))
-    #     print (url_for('profile' ,username='John Doe'))
-        
-        
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         return do_the_login()
+#     else: 
+#         return show_the_login_form()
     
-    app.run()
+
+""" static files """
+# url_for("static", filename="style.css")
+
+""" rendering templates"""
+
+from markupsafe import Markup
+
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
